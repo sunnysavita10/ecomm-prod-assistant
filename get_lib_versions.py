9e3e7d8cfc2b79d6302489b0373731c4d8b1e523
Sayed Ashfaq <302syedashfaq@gmail.com>
2025-09-24 15:49:21 +0530

    update get_lib_versions.py

--- get_lib_versions.py
+++ get_lib_versions.py
@@ -1,14 +1,21 @@
 import importlib.metadata
-packages = [
-    "langchain",
-    "python-dotenv",
-    "langchain_core",
-    "streamlit",
-    "langgraph"
-]
-for pkg in packages:
+from pathlib import Path
+
+file_path = Path.cwd()/ "requirements.txt"
+
+
+def get_requirements(file_path:str)->list[str]:
+    with open(file_path, 'r') as file:
+        requirements = file.readlines()
+        requirements = [req.split('==')[0] if "==" in req else req.replace('\n', '') for req in requirements]
+    return requirements
+
+packages = get_requirements(file_path)
+
+for package in packages:
     try:
-        version = importlib.metadata.version(pkg)
-        print(f"{pkg}=={version}")
+        version = importlib.metadata.version(package)
+        print(f"{package}== {version}")
     except importlib.metadata.PackageNotFoundError:
-        print(f"{pkg} (not installed)")
+        version = "not installed"
+        print(f"{package} ({version})")
